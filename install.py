caee03bd8348e2f2c5a7bf2094a32365fd9e044e
XFORWORKS <67760271+XFORWORKS@users.noreply.github.com>
2021-03-24 10:08:08 +0200

    Add files via upload

---
+++ install.py
@@ -0,0 +1,83 @@
+import sys
+import os
+
+def install():
+
+        os.system("sudo apt update")
+        os.system("sudo apt install pip")
+	os.system("pip install mechanize json whois python-whois requests bs4 requests[socks] urlparse cookielib") 
+	os.system("pip install scapy datetime argparse re threading urllib2 modules builtwith smtplib")
+	os.system("pip install whois")
+	os.system("pip install builtwith")
+	os.system("pip install colorama")
+	os.system("pip install dnspython")
+	os.system("pip install shodan")
+	os.system("sudo apt install python-socks -y")
+	os.system("sudo apt install nmap -y")
+	os.system("sudo apt install php -y")
+	os.system("sudo apt install perl -y")
+	os.system("sudo apt install hashcat -y")
+	os.system("sudo apt install nc")
+	os.system("sudo apt install neofetch")
+	os.system("sudo apt install cupp")
+        os.system("sudo apt install php")
+	os.system("cd /mnt/c")
+	os.system("cd /mnt/c && git clone https://github.com/thelinuxchoice/shellphish")
+	os.system("cd /mnt/c && git clone https://github.com/Tuhinshubhra/RED_HAWK")
+	os.system("cd /mnt/c && git clone https://github.com/XFORWORKS/TrackingScripts")
+	os.system("cd /mnt/c && git clone https://github.com/Mebus/cupp")
+        os.system("cd /mnt/c && git clone https://github.com/arismelachroinos/lscript")
+        os.system("cd /mnt/c && git clone https://github.com/trustedsec/social-engineer-toolkit")
+	os.system("apt install metasploit-framework -y")
+	os.system("apt install wifite -y")
+	os.system("apt install reaver -y")
+	os.system("apt install aircrack-ng -y")
+	os.system("cd /mnt/c/social-engineer-toolkit && pip install -r requirements.txt")
+	os.system("python /mnt/c/social-engineer-toolkit/setup.py install")
+
+                                                                                                         
+
+print "are you running on the real kali linux os   [y/n]"
+check = raw_input("[y/n]> ")
+if check == "y" :
+	print "ok most tools should work for you you might have to install other"
+	print "things on your os for this to work if it doesnt work"
+	os.system("sleep 2")
+	install()
+
+if check == "n" :
+	print "then some of the tools in this script might not work"
+	print "do you want to continue installation  [y/n]"
+	install = raw_input("[y/n]> ")
+	if install == "y" :
+                def install():
+                        os.system("sudo apt update")
+                        os.system("sudo apt install pip")
+			os.system("pip install mechanize json whois python-whois requests bs4 requests[socks] urlparse cookielib") 
+			os.system("pip install scapy datetime argparse re threading urllib2 modules builtwith smtplib")
+			os.system("pip install whois")
+			os.system("pip install builtwith")
+			os.system("sudo apt install python-socks -y")
+			os.system("sudo apt install nmap -y")
+			os.system("sudo apt install php -y")
+			os.system("sudo apt install perl -y")
+			os.system("sudo apt install hashcat")
+			os.system("sudo apt install nc")
+			os.system("sudo apt install neofetch")
+			os.system("sudo apt install cupp")
+			os.system("cd /mnt/c")
+                        os.system("cd /mnt/c && git clone https://github.com/thelinuxchoice/shellphish")
+                        os.system("cd /mnt/c && git clone https://github.com/Tuhinshubhra/RED_HAWK")
+                        os.system("cd /mnt/c && git clone https://github.com/XFORWORKS/TrackingScripts")
+                        os.system("cd /mnt/c && git clone https://github.com/Mebus/cupp")
+                        os.system("cd /mnt/c && git clone https://github.com/arismelachroinos/lscript")
+                        print "\n"
+			print "entering big download region prepare you anus if your not ready press ctrl C"
+			i = raw_input("press ctrl c to stop hit enter to continue")
+			os.system("sudo apt install metasploit-framework -y")
+			os.system("cd /mnt/c && git clone https://github.com/trustedsec/social-engineer-toolkit")
+			os.system("sudo apt install wifite -y")
+			os.system("sudo apt install reaver -y")
+			os.system("sudo apt install aircrack-ng -y")
+			os.system("cd /mnt/c/social-engineer-toolkit && pip install -r requirements.txt")
+			os.system("python /mnt/c/social-engineer-toolkit/setup.py install")
